c5ef5751a8d04f17f41908734c83f016e781296b
zujko <zujko@csh.rit.edu>
2015-11-05 23:17:37 -0500

    Added empty resize, blur, and brighten commands

--- imggo.go
+++ imggo.go
@@ -2,14 +2,47 @@ package main
 
 import (
 	"os"
+	"fmt"
 	"github.com/codegangsta/cli"
+	"github.com/fatih/color"
 )
 
 func main() {
 	app := cli.NewApp()
+	app.Author = "zujko"
 	app.Name = "img-go"
 	app.Usage = "INSERT USAGE HERE"
-	app.Action = func(c *cli.Context) {
+	app.Commands = []cli.Command{
+		{
+			Name: "resize",
+			Aliases: []string{"r"},
+			Usage: "resizes the image",
+			Action: func(c *cli.Context) {
+				if(len(c.Args()) >= 2) {
+					//RESIZE IMAGE
+				} else {
+					fmt.Println("usage: <image name> <size>")
+				}
+			},
+
+		},
+		{
+			Name: "blur",
+			Aliases: []string{"bl"},
+			Usage: "Adds a blur to the image",
+			Action: func(c *cli.Context) {
+				//BLUR IMAGE
+			},
+		},
+		{
+			Name: "brighten",
+			Aliases: []string{"br"},
+			Usage: "Brighten or darken the image",
+			Action: func(c *cli.Context) {
+				//BRIGHTEN IMAGE 
+			},
+		},
+
 	}
 	app.Run(os.Args)
 }
